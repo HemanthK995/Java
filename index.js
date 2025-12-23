60b16aa509cb7751987a938c24b31de9bb7e5ba6
HemanthK995 <Hemanthkala746@gmail.com>
2025-12-23 10:38:06 +0000

    added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function showMessage() {
+<<<<<<< HEAD
     document.getElementById("message").innerText = "Button clicked ";
+=======
+    document.getElementById("message").innerText = "Button clicked done!";
+>>>>>>> 080be7f (done was updated)
 }
